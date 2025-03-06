b2dce1781eba9771edf0845a5b33b5e0984cc9b7
saurabh singh rawat <saurabhgmis@gmail.com>
2025-03-06 12:58:53 +0530

    Home Page UI

--- Pages/homepage.php
+++ Pages/homepage.php
@@ -10,8 +10,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/homepage.css">
     <title>Home Page</title>
+    
+    <link rel="stylesheet" href="../CSS/navBar.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+
 </head>
 <body>
+
+ 
+    <header>
+        <a href="#" class="logo"><i class="ri-home-3-line"></i><span>Logo</span></a>
+
+        <ul class="navbar">
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="./Pages/aboutus.html">About Us</a></li>
+            <li><a href="./Pages/contactus.html">Contact Us</a></li>
+
+        </ul>
+
+        <div class="main">
+            <li class="dropdown">
+                <a href ="#" class="user"><i class="ri-user-3-line"></i>Profile</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">My Account</a></li>
+                    <li><a href="./donateNow.html">Donate</a></li>
+                    <li><a href="../index.html">Logout</a></li>
+                </ul>
+            </li>
+            <!-- <a href = "#">Register</a> -->
+            <div class="bx bx-menu" id="menu-icon"></div>
+
+        </div>
+    </header>
 
     <div class="popup" id="popup">
         <img src="../Assets/tick.png" alt="success">
